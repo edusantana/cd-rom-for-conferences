b020e8233a2b9ab1cc18a7bc85159cef49a2eb42
Eduardo de Santana Medeiros Alexandre <eduardo.ufpb@lavid.ufpb.br>
2013-10-29 01:29:04 -0300

    simple-cd files

--- test/simple-cd.rb
+++ test/simple-cd.rb
@@ -1,5 +1,8 @@
 require 'csv'
 require 'erubis'
+require 'logger'
+
+#log = Logger.new(STDOUT)
 
 input = STDIN.read
 eruby = Erubis::Eruby.new(input)      # create Eruby object
@@ -7,11 +10,29 @@ eruby = Erubis::Eruby.new(input)      # create Eruby object
 
 csv_options = {:headers => true, :col_sep => "\t", :encoding => 'UTF-8'}
 
+trabalhos = {}
+CSV.foreach("trabalhos.csv", csv_options) do |row|
+	categoria = trabalhos [ row[0] ] || []
+	categoria << row
+	trabalhos [row[0]] = categoria
+end
+
+equipes = {}
+CSV.foreach("equipe.csv", csv_options) do |row|
+	categoria = equipes [ row[0] ] || []
+	categoria << row
+	equipes [row[0]] = categoria
+end
+
+# log.error "Equipe: #{equipes.to_s}"
+
+
 ## create context object
 ## (key means var name, which may be string or symbol.)
 context = {
-  :t   => CSV.read("trabalhos.csv", csv_options),
-  'list' => ['aaa', 'bbb', 'ccc'],
+  :trabalhos   => trabalhos,
+  :equipes   => equipes,
+  :evento   => CSV.read("evento.csv", csv_options),
 }
 ## or
 # context = Erubis::Context.new()
